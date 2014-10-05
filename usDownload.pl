17f68b10b96e9b043846d8c00156ddab4f7f7b60
TakeAsh <take@TakeAsh.net>
2014-10-05 23:03:42 +0900

    複数チャンネル対応
    * perltidy 適用。.perltidyrc 追加。
    * log ファイルを保存するよう変更。

--- usDownload.pl
+++ usDownload.pl
@@ -10,86 +10,112 @@ use HTTP::Request;
 use YAML::Syck;
 
 $YAML::Syck::ImplicitUnicode = 1;
-$YAML::Syck::ImplicitTyping = 1;
+$YAML::Syck::ImplicitTyping  = 1;
 
 my $charsetConsole = 'UTF-8';
-my $charsetFile = 'UTF-8';
+my $charsetFile    = 'UTF-8';
 
-binmode(STDIN,  ":encoding($charsetConsole)");
-binmode(STDOUT, ":encoding($charsetConsole)");
-binmode(STDERR, ":encoding($charsetConsole)");
+binmode( STDIN,  ":encoding($charsetConsole)" );
+binmode( STDOUT, ":encoding($charsetConsole)" );
+binmode( STDERR, ":encoding($charsetConsole)" );
 
-my $configFile = $ARGV[0] or die($ARGV[0]);#|| 'config.yml';
-my $authUrl1 = 'https://www.ustream.tv/channel/';
-my $authUrl2 = '/0/video/download.rss';
-my $rssExt = 'rss';
+my $configFile = $ARGV[0] || 'config.yml';
+my $authUrl1   = 'https://www.ustream.tv/channel/';
+my $authUrl2   = '/0/video/download.rss';
+my $rssExt     = 'rss';
+my $logExt     = 'log';
 
 my $config = YAML::Syck::LoadFile($configFile) or die("$configFile: $!");
-my $path = $config->{'channel'};
-
-if (!(-d $path)){
-	mkdir($path) or die("$path: $!\n");
-}
 
 my $ua = LWP::UserAgent->new( keep_alive => 4, timeout => 300, );
-$ua->cookie_jar({});
+$ua->cookie_jar( {} );
 
-my $req = HTTP::Request->new(
-	GET => $authUrl1 . $config->{'channel'} . $authUrl2,
-);
-$req->authorization_basic($config->{'username'}, $config->{'password'});
-
-my $res = $ua->request($req);
-if (!$res->is_success){
-	die($res->status_line);
-} else {
-	print $config->{'channel'} . "\t" . $res->status_line. "\n";
+foreach my $channel ( @{ $config->{'channels'} } ) {
+    getChannel($channel);
 }
 
-my $rssXml = $res->decoded_content;
-open(my $fhRss, '>:utf8', "$path/$path.$rssExt") or die("$path.$rssExt: $!\n");
-print $fhRss $rssXml;
-close($fhRss);
+sub getChannel {
+    my $channel = shift or return;
 
-printf("No\tItems\tguid\tTitle\tType\tURL\tStatus\tSize\tTotal\t%%\n");
+    my $req = HTTP::Request->new( GET => $authUrl1 . $channel . $authUrl2, );
+    $req->authorization_basic( $config->{'username'}, $config->{'password'} );
 
-my $rss = XML::RSS->new;
-$rss->parse($rssXml);
-my @items = @{$rss->{'items'}};
-for(my $i=0; $i<@items; ++$i){
-	my $item = $items[$i];
-	my $guid = $item->{'guid'};
-	my $title = $item->{'title'};
-	my $type = $item->{'enclosure'}{'type'};
-	my $url = $item->{'enclosure'}{'url'};
-	$guid =~ s/^.+\/(\w+)$/$1/;
-	$type =~ s/^.+?([A-Za-z0-9]+)$/$1/;
-	my $fileName = "${guid}_$title.$type";
-	my $req = HTTP::Request->new(GET => $url);
-	my($status, $size, $total) = saveFile($req, "$path/$fileName");
-	my $percent = $total != 0 ? sprintf("%.1f" ,$size / $total * 100) : '-';
-	printf(
-		"%d\t%d\t%s\t%s\t%s\t%s\t%s\t%d\t%d\t%s\n",
-		$i+1, scalar(@items), $guid, $title, $type, $url, $status, $size, $total, $percent
-	);
+    my $res = $ua->request($req);
+    if ( !$res->is_success ) {
+        die( $res->status_line );
+    } else {
+        print "Channel:$channel\tStatus:$res->status_line\n";
+    }
+
+    if ( !( -d $channel ) ) {
+        mkdir($channel) or die("$channel: $!\n");
+    }
+
+    my $rssXml = $res->decoded_content;
+    open( my $fhRss, '>:utf8', "$channel/$channel.$rssExt" )
+        or die("$channel.$rssExt: $!\n");
+    print $fhRss $rssXml;
+    close($fhRss);
+
+    open( my $fhLog, '>:utf8', "$channel/$channel.$logExt" )
+        or die("$channel.$logExt: $!\n");
+
+    my $rss = XML::RSS->new;
+    $rss->parse($rssXml);
+    my @items = @{ $rss->{'items'} };
+    for ( my $i = 0; $i < @items; ++$i ) {
+        my $item  = $items[$i];
+        my $guid  = $item->{'guid'};
+        my $title = $item->{'title'};
+        my $type  = $item->{'enclosure'}{'type'};
+        my $url   = $item->{'enclosure'}{'url'};
+        $guid =~ s/^.+\/(\w+)$/$1/;
+        $type =~ s/^.+?([A-Za-z0-9]+)$/$1/;
+        my $fileName = "${guid}_$title.$type";
+        my $req = HTTP::Request->new( GET => $url );
+        my ( $status, $size, $total )
+            = saveFile( $req, "$channel/$fileName" );
+        my $percent
+            = $total != 0
+            ? sprintf( "%.1f", $size / $total * 100 )
+            : '-';
+        printf $fhLog (
+            "No:%d\tItems:%d\tguid:%s\tTitle:%s\t"
+                . "Type:%s\tURL:%s\tStatus:%s\tSize:%d\t"
+                . "Total:%d\tPercent:%s\n",
+            $i + 1,
+            scalar(@items),
+            $guid,
+            $title,
+            $type,
+            $url,
+            $status,
+            $size,
+            $total,
+            $percent
+        );
+    }
+
+    close($fhLog);
 }
 
 sub saveFile {
-	my($req, $filename) = @_;
-	my $retry = 0;
-	my $size = 0;
-	my $total = 0;
-	while(1){
-		my $res = $ua->request($req, $filename);
-		$size = -s $filename || 0;
-		$total = $res->header('Content-Length') || 0;
-		if (++$retry <= 10 && $size < $total){
-			sleep(30);
-		} else {
-			last;
-		}
-	}
-	return ($res->status_line, $size, $total);
+    my ( $req, $filename ) = @_;
+    my $retry = 0;
+    my $res;
+    my $size  = 0;
+    my $total = 0;
+    while (1) {
+        $res = $ua->request( $req, $filename );
+        $size = -s $filename || 0;
+        $total = $res->header('Content-Length') || 0;
+        if ( ++$retry <= 10 && $size < $total ) {
+            sleep(30);
+        } else {
+            last;
+        }
+    }
+    return ( $res->status_line, $size, $total );
 }
 
 # EOF
